3ea9a56563bf646f2c5ec39c6150d5a72ede6f6e
aadolpho <aadolpho@ucsc.edu>
2021-10-14 04:03:12 -0700

    Completed all the tasks for Lab 4!

---
+++ lab4/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link URL
+ * @file This file defines the Myclass class.
+ * @author AuthorName.
+ * @since x.x.x
+ */
+
+ /** jshint {inline configuration} */
+
+//Creating a blackjack game
+  //Print out a statement that prompts the user to pass or add a card.
+  //Accept the user output.
+    //If the user’s hand is over 21, the player loses.
+    //Else, if the user’s hand is under 21, prompt the user to pass or add a card to their hand.
+  //Repeat step 3.
+    //If the user decides to pass, the computer must do the same with steps 1-5.
+    //If the player and computer decides to pass, compare both hands.
+  //If the player 's hand is greater than the computer's hand, then the player wins.
+  //Else, if the computer’s hnd is greater than the player’s hand, then the computer wins.
+  //Prompt the player to play again.

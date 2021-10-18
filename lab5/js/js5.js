dc0b33055532799e1c681283fde04246b614f4d0
aadolpho <aadolpho@ucsc.edu>
2021-10-18 15:54:41 -0700

    Lab 5 tasks 1-3

---
+++ lab5/js/js5.js
@@ -0,0 +1,24 @@
+/*
+  * Author: Alexis Adolpho <aadolpho@ucsc.edu>
+  * Created: 19 October
+  * License: Public Domain
+  */
+
+//Define Variables
+
+make = "Honda";
+model = "Accord";
+color = "Black";
+year = 2000;
+
+//Calculate
+
+age = 2021 - year;
+
+//Output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");

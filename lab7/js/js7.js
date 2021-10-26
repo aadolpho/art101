d1d08f91e2c2d209b87251dad790ab27c49ddeb4
aadolpho <aadolpho@ucsc.edu>
2021-10-26 14:45:28 -0700

    Lab 7 Finished Task 2

---
+++ lab7/js/js7.js
@@ -0,0 +1,29 @@
+/*
+  * Author: Alexis Adolpho <aadolpho@ucsc.edu>
+  * Created: 26 October
+  * License: Public Domain
+  */
+
+//Creating a function where user inputs their name & sorts the letters
+//alphabetically
+
+function sortName() {
+  var userName = window.prompt("Hello! Let's sort your name. What is your name?");
+  console.log("Your username =", userName);
+
+  var nameArray = userName.split('');             //We're going to split the name first into individual letters
+  console.log("nameArray =", nameArray);
+
+  var nameArraySort = nameArray.sort();           //After splitting the name, we will sort the letters alphabetically
+  console.log("nameArraySort =", nameArraySort);
+
+  var nameSorted = nameArraySort.join('');        //Once we are finished sorting, we will join the letters together
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted;                              //Once, we are finished, we will return the new, sorted name
+}
+
+//Output
+
+document.writeln("We are finished! Here is your new name: ",
+  sortName(), "</br>");

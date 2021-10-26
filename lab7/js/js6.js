99182021c05d8598d05fc86a1f04aef87039ff5d
aadolpho <aadolpho@ucsc.edu>
2021-10-26 14:24:57 -0700

    Lab 7 task 1 completed

---
+++ lab7/js/js6.js
@@ -0,0 +1,25 @@
+/*
+  * Author: Alexis Adolpho <aadolpho@ucsc.edu>
+  * Created: 21 October
+  * License: Public Domain
+  */
+
+//Define Array
+
+myTransport = ["Honda Accord", " Rides from Friends", " Pet Dragon"];
+
+//Define Object
+
+myMainRide = {
+  make : "Honda",
+  model : "Accord",
+  color: "Black",
+  year : 2000,
+  age : 21
+  }
+
+//Output
+
+document.writeln("Getting around: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");

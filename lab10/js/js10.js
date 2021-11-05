0176ef62496d5a0d92a2c67efa27d4e4b0c02fc9
aadolpho <aadolpho@ucsc.edu>
2021-11-04 20:58:04 -0700

    Lab10 Tasks Update

---
+++ lab10/js/js10.js
@@ -0,0 +1,31 @@
+/*
+  * Author: Alexis Adolpho <aadolpho@ucsc.edu>
+  * Created: 2 November
+  * License: Public Domain
+  */
+
+  //Creating a function where user inputs their name & sorts the letters
+//alphabetically
+
+document.getElementById("my-button").addEventListener("click", display)
+
+function sortName() {
+  var userName = window.prompt("Hello! Let's sort your name. What is your name?");
+  console.log("userName =", userName);
+
+  var nameArray = userName.split('');             
+  console.log("nameArray =", nameArray);
+
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted;
+}
+
+//Output
+
+document.writeln("We are finished! Here is your new name: ",
+    sortName(), "</br>");

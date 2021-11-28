5baf426024350f033777b865f2f59694cf95affe
aadolpho <aadolpho@ucsc.edu>
2021-11-28 00:35:32 -0800

    Lab13: Finished Task 1

---
+++ lab12/js/js123.js
@@ -0,0 +1,32 @@
+/*
+  * Author: Alexis Adolpho <aadolpho@ucsc.edu>
+  * Created: 16 November
+  * License: Public Domain
+  */
+
+//Depending on the length of mod 4, we will be returning either Gryffindor, Ravenclaw, Slytherin, or Hufflepuff
+
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
